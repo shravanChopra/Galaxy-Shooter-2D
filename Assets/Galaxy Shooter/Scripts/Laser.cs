ea4cc6a461317a3b1888aeb110f9f86d8adee7f5
Shravan Chopra <shrav_20@hotmail.com>
2017-10-22 17:56:16 -0500

    Implemented basic laser shooting system

---
+++ Assets/Galaxy Shooter/Scripts/Laser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+	[SerializeField] private float _speed = 10f;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+		// destroy the laser when it moves offscreen
+		if (transform.position.y >= 5.4f)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
--- Assets/Galaxy Shooter/Scripts/Player.cs
+++ Assets/Galaxy Shooter/Scripts/Player.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-	[SerializeField] private float speed = 5.0f;
-	
+	[SerializeField] private float _speed = 5.0f;
+	[SerializeField] private float _fireRate = 0.25f;
+	private float _nextFireTime = 0.0f;
+	[SerializeField] private GameObject _laserPrefab;
+
 	// Use this for initialization
 	void Start ()
 	{
@@ -14,15 +17,19 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		Movement();
-		
-	}
-	
-	private void Movement()
+		ControlMovement();
+
+		if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+		{
+			Shoot();			
+		}
+	}	
+
+	private void ControlMovement ()
 	{
 		// enable vertical and horizontal movement through user input
-		transform.Translate(Vector3.right * speed * Input.GetAxis("Horizontal") * Time.deltaTime);
-		transform.Translate(Vector3.up * speed * Input.GetAxis("Vertical") * Time.deltaTime);
+		transform.Translate(Vector3.right * _speed * Input.GetAxis("Horizontal") * Time.deltaTime);
+		transform.Translate(Vector3.up * _speed * Input.GetAxis("Vertical") * Time.deltaTime);
 
 		// set x bounds - let's wrap around!
 		if (transform.position.x > 9.4f)
@@ -43,5 +50,13 @@ public class Player : MonoBehaviour {
 		{
 			transform.position = new Vector3(transform.position.x, -4.2f, transform.position.z);
 		}		
+	}
+	private void Shoot ()
+	{
+		if (Time.time > _nextFireTime)
+		{
+			Instantiate(_laserPrefab, transform.position + Vector3.up, Quaternion.identity);
+			_nextFireTime = Time.time + _fireRate;
+		}	
 	}
 }
